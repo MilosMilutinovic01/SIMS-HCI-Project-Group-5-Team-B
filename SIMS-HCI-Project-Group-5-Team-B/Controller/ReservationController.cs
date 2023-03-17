bb60d25b10d2e69fb2dad703a623df5747955ba7
Jelena Kovač <jelena.kovac765@gmail.com>
2023-03-17 16:23:16 +0100

    Reservation Recommendation and Creation

--- SIMS-HCI-Project-Group-5-Team-B/Controller/ReservationController.cs
+++ SIMS-HCI-Project-Group-5-Team-B/Controller/ReservationController.cs
@@ -8,6 +8,18 @@ using SIMS_HCI_Project_Group_5_Team_B.Model;
 
 namespace SIMS_HCI_Project_Group_5_Team_B.Controller
 {
+    public struct ReservationRecommendation
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }   
+
+       public ReservationRecommendation(DateTime start, DateTime end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+    }
+
     public class ReservationController
     {
         private Repository<Reservation> reservationRepository;
@@ -56,6 +68,91 @@ namespace SIMS_HCI_Project_Group_5_Team_B.Controller
                     reservation.Accommodation = accommodation;
                 }
             }
+        }
+
+        public List<ReservationRecommendation> GetReservationRecommendations(Accommodation selectedAccommodation, DateTime startDate, DateTime endDate, int reservationDays)
+        {
+            List<ReservationRecommendation> reservationRecommendations = new List<ReservationRecommendation>();
+            reservationRecommendations = GetRecommendationsInTimeSpan(selectedAccommodation, startDate, endDate, reservationDays);
+            if(reservationRecommendations.Count == 0) 
+            {
+                reservationRecommendations = GetRecommendationsOutOfTimeSpan(selectedAccommodation, startDate.Date, endDate.Date, reservationDays);
+            }
+
+            return reservationRecommendations;
+        }
+
+        private List<ReservationRecommendation> GetRecommendationsInTimeSpan(Accommodation selectedAccommodation, DateTime startDate, DateTime endDate, int reservationDays)
+        {
+            List<ReservationRecommendation> reservationRecommendations = new List<ReservationRecommendation>();
+            DateTime start = startDate;
+            DateTime end = startDate;
+            while(start.AddDays(reservationDays - 1) <= endDate)
+            {
+                end = start.AddDays(reservationDays - 1);
+                if (IsAccomodationAvailable(selectedAccommodation, start, end))
+                {
+                    reservationRecommendations.Add(new ReservationRecommendation( start, end));
+                }
+                start = start.AddDays(1);
+            }
+
+            return reservationRecommendations;
+        }
+
+        private List<ReservationRecommendation> GetRecommendationsOutOfTimeSpan(Accommodation selectedAccommodation, DateTime startDate, DateTime endDate, int reservationDays)
+        {
+            List<ReservationRecommendation> reservationRecommendations = new List<ReservationRecommendation>();
+            DateTime start = endDate.AddDays(1);
+            DateTime end = endDate.AddDays(1);
+            int count = 0;
+            while (count != 3)
+            {
+                end = start.AddDays(reservationDays - 1);
+                if (IsAccomodationAvailable(selectedAccommodation, start, end))
+                {
+                    reservationRecommendations.Add(new ReservationRecommendation(start, end));
+                    count++;
+                }
+                start = start.AddDays(1);
+            }
+
+            return reservationRecommendations;
+        }
+
+        public List<Reservation> GetAccomodationReservations(Accommodation selectedAccommodation)
+        {
+            List<Reservation> accomodationReservations = new List<Reservation>();
+            foreach (Reservation reservation in GetAll())
+            {
+                if (reservation.AccommodationId == selectedAccommodation.Id)
+                {
+                    accomodationReservations.Add(reservation);
+                }
+            }
+            return accomodationReservations;
+        }
+
+        public bool IsAccomodationAvailable(Accommodation selectedAccommodation, DateTime startDate, DateTime endDate)
+        {
+            List<Reservation> accomodationReservations = GetAccomodationReservations(selectedAccommodation);
+
+            foreach (Reservation reservation in accomodationReservations)
+            {
+                if (startDate >= reservation.StartDate && startDate <= reservation.EndDate)
+                {
+                    return false;
+                }
+                else if (endDate >= reservation.StartDate && endDate <= reservation.EndDate)
+                {
+                    return false;
+                }
+                else if (startDate <= reservation.StartDate && endDate >= reservation.EndDate)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
