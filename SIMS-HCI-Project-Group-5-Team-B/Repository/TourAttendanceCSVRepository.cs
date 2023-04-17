18bac884013a26f597dac0849c6efcbef587c2dd
ukinolo <ykithethird@gmail.com>
2023-04-17 21:52:20 +0200

    Implementing all added repository interfaces

--- SIMS-HCI-Project-Group-5-Team-B/Repository/TourAttendanceCSVRepository.cs
+++ SIMS-HCI-Project-Group-5-Team-B/Repository/TourAttendanceCSVRepository.cs
@@ -8,33 +8,82 @@ using System.Threading.Tasks;
 
 namespace SIMS_HCI_Project_Group_5_Team_B.Repository
 {
-    public class TourAttendanceCSVRepository : ITourAttendanceRepository
+    public class TourAttendanceCSVRepository : CSVRepository<TourAttendance>, ITourAttendanceRepository
     {
-        private Repository<TourAttendance> tourAttendanceRepository;
-
-        public TourAttendanceCSVRepository()
-        {
-            tourAttendanceRepository = new Repository<TourAttendance>();
-        }
+        public TourAttendanceCSVRepository() : base() { }
 
         public void Delete(TourAttendance tourAttendance)
         {
-            tourAttendanceRepository.Delete(tourAttendance);
+            TourAttendance? founded = _data.Find(d => d.Id == tourAttendance.Id);
+            if (founded != null)
+            {
+                _data.Remove(founded);
+                WriteCSV(_data);
+            }
         }
 
         public List<TourAttendance> GetAll()
         {
-            return tourAttendanceRepository.GetAll();
+            return _data;
         }
 
         public void Save(TourAttendance newTourAttendance)
         {
-            tourAttendanceRepository.Save(newTourAttendance);
+            newTourAttendance.Id = NextId();
+            _data.Add(newTourAttendance);
+            WriteCSV(_data);
+        }
+        private int NextId()
+        {
+            if (_data.Count() < 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return _data.Max(d => d.Id) + 1;
+            }
         }
 
         public void Update(TourAttendance tourAttendance)
         {
-            tourAttendanceRepository.Update(tourAttendance);
+            TourAttendance? current = _data.Find(d => d.Id == tourAttendance.Id);
+            if (current == null)
+            {
+                Save(tourAttendance);
+                return;
+            }
+            int index = _data.IndexOf(current);
+            _data.Remove(current);
+            _data.Insert(index, tourAttendance);
+            WriteCSV(_data);
+        }
+
+        protected override TourAttendance FromCSV(string[] values)
+        {
+            TourAttendance newTourAttendance = new TourAttendance();
+
+            newTourAttendance.Id = int.Parse(values[0]);
+            newTourAttendance.GuideGuestId = int.Parse(values[1]);
+            newTourAttendance.AppointmentId = int.Parse(values[2]);
+            newTourAttendance.PeopleAttending = int.Parse(values[3]);
+            newTourAttendance.KeyPointGuestArrivedId = int.Parse(values[4]);
+
+            return newTourAttendance;
+        }
+
+        protected override string[] ToCSV(TourAttendance obj)
+        {
+            string[] csvValues =
+            {
+                obj.Id.ToString(),
+                obj.GuideGuestId.ToString(),
+                obj.AppointmentId.ToString(),
+                obj.PeopleAttending.ToString(),
+                obj.KeyPointGuestArrivedId.ToString()
+            };
+
+            return csvValues;
         }
     }
 }
