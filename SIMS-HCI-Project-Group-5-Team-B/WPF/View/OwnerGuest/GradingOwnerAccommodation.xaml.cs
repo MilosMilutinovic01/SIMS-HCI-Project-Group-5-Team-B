4edc6d9ffabb4192ef273777a788748a1978f672
Jelena Kovač <jelena.kovac765@gmail.com>
2023-05-07 00:23:53 +0200

    GradingOwnerAccommodation adjustment to MVVM

--- SIMS-HCI-Project-Group-5-Team-B/WPF/View/OwnerGuest/GradingOwnerAccommodation.xaml.cs
+++ SIMS-HCI-Project-Group-5-Team-B/WPF/View/OwnerGuest/GradingOwnerAccommodation.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using SIMS_HCI_Project_Group_5_Team_B.Application.UseCases;
 using SIMS_HCI_Project_Group_5_Team_B.Domain.Models;
+using SIMS_HCI_Project_Group_5_Team_B.Utilities;
 using SIMS_HCI_Project_Group_5_Team_B.WPF.View.OwnerGuest;
 using SIMS_HCI_Project_Group_5_Team_B.WPF.ViewModel;
 using System.Collections.ObjectModel;
@@ -12,80 +13,14 @@ namespace SIMS_HCI_Project_Group_5_Team_B.View
     /// </summary>
     public partial class GradingOwnerAccommodation : Window
     {
-        //property for heading text
-        public string Heading { get; set; }
-        public Reservation SelectedReservation { get; set; }
-        private SingleReservationViewModel reservationView;
-        public OwnerAccommodationGrade OwnerAccommodationGrade { get; set; }
-        private OwnerAccommodationGradeSevice ownerAccommodationGradeService;
-        private ReservationService reservationService;
-        //private ObservableCollection<ReservationGridView> ReservationViews;
-
-        
-        private SuperOwnerService superOwnerService;
-        private OwnerService ownerService;
+       
         public GradingOwnerAccommodation(OwnerAccommodationGradeSevice ownerAccommodationGradeService, ReservationService reservationService, SingleReservationViewModel reservationView, SuperOwnerService superOwnerService, OwnerService ownerService)
         {
             InitializeComponent();
-            this.DataContext = this;
-            OwnerAccommodationGrade = new OwnerAccommodationGrade(1,1,1,1,1);
-            this.reservationView = reservationView;
-            this.SelectedReservation = reservationView.Reservation;
-            this.ownerAccommodationGradeService = ownerAccommodationGradeService;
-            this.reservationService = reservationService;
-            this.superOwnerService = superOwnerService;
-            this.ownerService = ownerService;
-            Heading = string.Empty;
-            FormHeading();
+            this.DataContext = new GradingOwnerAccommodationViewModel(ownerAccommodationGradeService,reservationService,reservationView,superOwnerService,ownerService,this);
+           
             
         }
 
-        private void FormHeading()
-        {
-            Heading = SelectedReservation.Accommodation.Name + " Grading\n"
-                      + "Owner: " + SelectedReservation.Accommodation.Owner.Name + " " + SelectedReservation.Accommodation.Owner.Surname + "";
-        }
-
-        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void Grade_Button_Click(object sender, RoutedEventArgs e)
-        {
-            if(OwnerAccommodationGrade.IsValid)
-            {
-                //setting parameter to true
-                SelectedReservation.IsGradedByGuest = true;
-                reservationService.Update(SelectedReservation);
-                OwnerAccommodationGrade.ReservationId = SelectedReservation.Id;
-                OwnerAccommodationGrade.GradeAverage = ownerAccommodationGradeService.GetAverageGrade(OwnerAccommodationGrade);
-                ownerAccommodationGradeService.Save(OwnerAccommodationGrade);
-                reservationView.IsForGrading = false;
-                OwnerAccommodationGrade.Reservation.Accommodation.Owner.GradeAverage = superOwnerService.CalculateGradeAverage(OwnerAccommodationGrade.Reservation.Accommodation.Owner);
-                if (OwnerAccommodationGrade.Reservation.Accommodation.Owner.GradeAverage > 4.5 && superOwnerService.GetNumberOfGrades(OwnerAccommodationGrade.Reservation.Accommodation.Owner) >= 50)
-                {
-                    OwnerAccommodationGrade.Reservation.Accommodation.Owner.IsSuperOwner = true;
-                }
-                else
-                {
-                    OwnerAccommodationGrade.Reservation.Accommodation.Owner.IsSuperOwner = false;
-                }
-                ownerService.Update(OwnerAccommodationGrade.Reservation.Accommodation.Owner);
-                MessageBox.Show("Grading was successful!");
-                
-                Close();
-            }
-            else
-            {
-                MessageBox.Show("Data is not valid!");
-            }
-        }
-
-        private void RenovationRequest_Button_Click(object sender, RoutedEventArgs e)
-        {
-            RenovationRequestForm renovationRequestForm = new RenovationRequestForm(SelectedReservation.Id);
-            renovationRequestForm.Show();
-        }
     }
 }
