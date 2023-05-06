d79d42c3e06bdbce30165e38284bc6ff009d43dd
Jelena Kovač <jelena.kovac765@gmail.com>
2023-05-06 20:54:09 +0200

    AccommodationsWindow Adjustments to MVVM

--- SIMS-HCI-Project-Group-5-Team-B/WPF/View/OwnerGuest/AccommodationsPage.xaml.cs
+++ SIMS-HCI-Project-Group-5-Team-B/WPF/View/OwnerGuest/AccommodationsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using SIMS_HCI_Project_Group_5_Team_B.Application.UseCases;
 using SIMS_HCI_Project_Group_5_Team_B.Controller;
 using SIMS_HCI_Project_Group_5_Team_B.Domain.Models;
+using SIMS_HCI_Project_Group_5_Team_B.WPF.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -15,244 +16,17 @@ namespace SIMS_HCI_Project_Group_5_Team_B.View
     /// <summary>
     /// Interaction logic for AccomodationsWindow.xaml
     /// </summary>
-    public partial class AccommodationsPage : Page, INotifyPropertyChanged, IDataErrorInfo
+    public partial class AccommodationsPage : Page
     {
-        private AccommodationService accommodationService;
-        private LocationController locationController;
-        private ReservationService reservationService;
-        private OwnerService ownerService;
-        private OwnerAccommodationGradeSevice ownerAccommodationGradeService;
-        private SuperOwnerService superOwnerService;
-        private RenovationService renovationService;
-        public ObservableCollection<Accommodation> Accomodations { get; set; }
-        public Accommodation SelectedAccommodation { get; set; }
-        public string SearchName { get; set; } = "";
-        public string City { get; set; }
-        private string state;
-        public string State 
-        { 
-            get
-            {
-                return state;
-            }
-            set
-            {
-                state = value;
-                NotifyPropertyChanged(nameof(State));
-                NotifyPropertyChanged(nameof(City));
-            } 
-        
-        }
-        public List<string> states { get; set; }
-        public List<string> cities { get; set; }
-        public string SearchType { get; set; } = "";
-        public string SearchGuestsNumber { get; set; } = "";
-        public string SearchDays { get; set; } = "";
-
-        public string Error => null;
-
-        private int ownerGuestId;
+       
 
         public AccommodationsPage(int ownerGuestId)
         {
             InitializeComponent();
-            DataContext = this;
-            locationController = new LocationController();
-            ownerService = new OwnerService();
-            renovationService = new RenovationService();
-            renovationService.MarkRenovatiosThatTookPlaceInTheLastYear();
-            this.accommodationService = new AccommodationService(locationController, ownerService);
-            this.reservationService = new ReservationService(this.accommodationService);
-            //Accomodations = new ObservableCollection<Accommodation>(accommodationController.GetAll());
-            ownerAccommodationGradeService = new OwnerAccommodationGradeSevice(this.reservationService);
-            superOwnerService = new SuperOwnerService(ownerAccommodationGradeService, this.accommodationService);
-           // renovationService = new RenovationService();
-            //renovationService.MarkRenovatiosThatTookPlaceInTheLastYear();
-            Accomodations = new ObservableCollection<Accommodation>(superOwnerService.GetSortedAccommodations());
-            this.ownerGuestId = ownerGuestId;
-            //reservationController = new ReservationController(accommodationController);
-            states = locationController.GetStates();
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-        private void NotifyPropertyChanged(string info)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(info));
-            }
-        }
-
-        private void Cancel_Button_Click(object sender, RoutedEventArgs e)
-        {
-           // Close();
-        }
-
-        private void Search_Button_Click(object sender, RoutedEventArgs e)
-        {
-            List<Accommodation> searchResult;
-            if (this.IsValid)
-            {
-                
-                if (IsSearchParameter(SearchGuestsNumber) && IsSearchParameter(SearchDays))
-                {
-                    searchResult = accommodationService.GetSearchResult(FindLocationId(),SearchName,SearchType, Int32.Parse(SearchGuestsNumber), Int32.Parse(SearchDays));
-                }
-                else if (IsSearchParameter(SearchGuestsNumber) && !IsSearchParameter(SearchDays))
-                {
-                    searchResult = accommodationService.GetSearchResult(FindLocationId(), SearchName, SearchType, Int32.Parse(SearchGuestsNumber));
-                }
-                else if (!IsSearchParameter(SearchGuestsNumber) && IsSearchParameter(SearchDays))
-                {
-                    searchResult = accommodationService.GetSearchResult(FindLocationId(), SearchName, SearchType, 1, Int32.Parse(SearchDays));
-                }
-                else
-                {
-                    searchResult = accommodationService.GetSearchResult(FindLocationId(), SearchName, SearchType);
-                }
-
-                if (searchResult != null)
-                {
-                    Accomodations.Clear();
-                    foreach (Accommodation accommodation in searchResult)
-                    {
-                        Accomodations.Add(accommodation);
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show("Search can not be preformed beacause data is not valid!");
-            }
-        }
-
-        private void DataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            if (SelectedAccommodation != null)
-            {
-                AccomodationDetailsWindow accomodationDetailsWindow = new AccomodationDetailsWindow(SelectedAccommodation, reservationService, ownerGuestId);
-                accomodationDetailsWindow.Show();
-            }
-        }
-        Regex numberRegex = new Regex(@"[\d]");
-
-        public string this[string columnName]
-        {
-            get
-            {
-                if (columnName == "State")
-                {
-                    if (string.IsNullOrEmpty(State))
-                    {
-                        return null;
-                    }
-                    
-                }
-                
-
-                if (columnName == "City")
-                {
-                    if (!string.IsNullOrEmpty(State) && string.IsNullOrEmpty(City))
-                    {
-                        return "You must select city";
-                    }
-                    else
-                    {
-                        return null;
-                    }
-
-
-                }
-                else if (columnName == "SearchGuestsNumber")
-                {
-                    if (string.IsNullOrEmpty(SearchGuestsNumber))
-                    {
-                        return null;
-                    }
-                    Match match = numberRegex.Match(SearchGuestsNumber);
-                    if (!match.Success)
-                        return "Invalid input";
-                }
-                else if (columnName == "SearchDays")
-                {
-                    if (string.IsNullOrEmpty(SearchDays))
-                    {
-                        return null;
-                    }
-                    Match match = numberRegex.Match(SearchDays);
-                    if (!match.Success)
-                        return "Invalid input";
-                }
-                return null;
-
-
-            }
-        }
-        private readonly string[] _validatedProperties = { "City","State", "SearchGuestsNumber", "SearchDays" };
-        public bool IsValid
-        {
-            get
-            {
-                foreach (var property in _validatedProperties)
-                {
-                    if (this[property] != null)
-                        return false;
-                }
-
-                return true;
-            }
-        }
-
-        private bool IsSearchParameter(string parameter)
-        {
-            return !string.IsNullOrEmpty(parameter);
-        }
-
-        private int FindLocationId()
-        {
-            if (string.IsNullOrEmpty(this.State) || string.IsNullOrEmpty(this.City))
-            {
-                return -1;
-            }
+            DataContext = new AccommodationsViewModel(ownerGuestId);
             
-            string State = this.State;
-            string City = this.City;
-
-            int locationId = -2;
-            foreach (Location location in locationController.GetAll())
-            {
-                if (location.State == State && location.City == City)
-                {
-                    locationId = location.Id;
-                    break;
-                }
-            }
-
-            return locationId;
         }
 
-        private void Reset_Button_Click(object sender, RoutedEventArgs e)
-        {
-            Accomodations.Clear();
-            foreach (Accommodation accommodation in accommodationService.GetAll())
-            {
-                Accomodations.Add(accommodation);
-            }
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            cities = locationController.GetCityByState(ComboBoxStates.SelectedItem.ToString());
-            ComboBoxCities.ItemsSource = cities;
-        }
-
-        private void Details_Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (SelectedAccommodation != null)
-            {
-                AccomodationDetailsWindow accomodationDetailsWindow = new AccomodationDetailsWindow(SelectedAccommodation, reservationService, ownerGuestId);
-                accomodationDetailsWindow.Show();
-            }
-        }
+       
     }
 }
