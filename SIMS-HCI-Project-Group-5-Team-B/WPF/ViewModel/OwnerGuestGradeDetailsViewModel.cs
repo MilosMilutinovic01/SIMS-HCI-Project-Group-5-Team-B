13c2550029bbb38fb1b99bd113af5165cc96336f
Jelena Kovač <jelena.kovac765@gmail.com>
2023-05-06 23:36:21 +0200

    GradeDetailsWindow adjustment to MVVM

--- SIMS-HCI-Project-Group-5-Team-B/WPF/ViewModel/OwnerGuestGradeDetailsViewModel.cs
+++ SIMS-HCI-Project-Group-5-Team-B/WPF/ViewModel/OwnerGuestGradeDetailsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using SIMS_HCI_Project_Group_5_Team_B.DTO;
+using SIMS_HCI_Project_Group_5_Team_B.Utilities;
+using SIMS_HCI_Project_Group_5_Team_B.WPF.View.OwnerGuest;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +15,29 @@ namespace SIMS_HCI_Project_Group_5_Team_B.WPF.ViewModel
         public string GuestName { get; set; }
         public string Start { get; set; }
         public string End { get; set; }
-        public OwnerGuestGradeDetailsViewModel(OwnerGuestGradesDTO SelectedGrade) 
+        public RelayCommand CloseCommand { get;}
+        protected GradeDetailsWindow window;
+        public OwnerGuestGradeDetailsViewModel(OwnerGuestGradesDTO SelectedGrade, GradeDetailsWindow window) 
         {
             this.SelectedGrade = SelectedGrade;
             GuestName = SelectedGrade.Grade.Reservation.OwnerGuest.Name + " " + SelectedGrade.Grade.Reservation.OwnerGuest.Surname;
             Start = SelectedGrade.Grade.Reservation.StartDate.ToShortDateString();
             End = SelectedGrade.Grade.Reservation.EndDate.ToShortDateString();
+            this.window = window;
+
+            //commands
+            CloseCommand = new RelayCommand(Cancel_Execute,CanExecute);
+
+        }
+
+        public bool CanExecute()
+        {
+            return true;
+        }
+
+        public void Cancel_Execute()
+        {
+            window.Close();
         }
     }
 }
