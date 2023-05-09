9e24925ebdf1ba8dd444d8c551f81fa0606466bb
ukinolo <ykithethird@gmail.com>
2023-05-09 10:23:57 +0200

    Added regular tour request basic form

---
+++ SIMS-HCI-Project-Group-5-Team-B/WPF/View/GuideGuest/UserControls/RegularTourRequestForm.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SIMS_HCI_Project_Group_5_Team_B.WPF.View.GuideGuest.UserControls
+{
+    /// <summary>
+    /// Interaction logic for RegularTourRequestForm.xaml
+    /// </summary>
+    public partial class RegularTourRequestForm : UserControl
+    {
+        public RegularTourRequestForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
