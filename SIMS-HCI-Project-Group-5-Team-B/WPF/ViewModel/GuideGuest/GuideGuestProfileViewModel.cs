8c57a2932bf086388bbc9a6831f465b655163c03
ukinolo <ykithethird@gmail.com>
2023-05-11 15:55:11 +0200

    Fixed command binding

---
+++ SIMS-HCI-Project-Group-5-Team-B/WPF/ViewModel/GuideGuest/GuideGuestProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SIMS_HCI_Project_Group_5_Team_B.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS_HCI_Project_Group_5_Team_B.WPF.ViewModel.GuideGuest
+{
+    public class GuideGuestProfileViewModel
+    {
+        public ObservableCollection<Voucher> Vouchers { get; set; }
+
+
+        public GuideGuestProfileViewModel()
+        {
+            Vouchers = new ObservableCollection<Voucher>();
+        }
+    }
+}
--- SIMS-HCI-Project-Group-5-Team-B/WPF/ViewModel/GuideGuest/TourSearchPageViewModel.cs
+++ SIMS-HCI-Project-Group-5-Team-B/WPF/ViewModel/GuideGuest/TourSearchPageViewModel.cs
@@ -8,20 +8,25 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Data;
 using Xceed.Wpf.Toolkit.Primitives;
 
 namespace SIMS_HCI_Project_Group_5_Team_B.WPF.ViewModel.GuideGuest
 {
-    public class TourSearchPageViewModel
+    public class TourSearchPageViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<Tour> Tours { get; set; }
 
 
-        public List<string> States { get; set; }
-        private string selectedState;
+        public ObservableCollection<string> States { get; set; }
+        public ObservableCollection<string> Cities { get; set; }
+        
+        
+        private string selectedState = string.Empty;
         public string SelectedState
         {
             get => selectedState;
@@ -29,31 +34,81 @@ namespace SIMS_HCI_Project_Group_5_Team_B.WPF.ViewModel.GuideGuest
             {
                 selectedState = value;
                 Cities.Clear();
-                foreach(var city in locationController.GetCityByState(selectedState))
+                foreach (var city in locationService.GetCityByState(selectedState))
                 {
                     Cities.Add(city);
                 }
+                OnPropertyChanged();
             }
         }
-        public ObservableCollection<String> Cities { get; set; }
-        public string SelectedCity { get; set; }
-        public int NumberOfGuests { get; set; }
-        public string Language { get; set; }
-        public int Duration { get; set; }
+        private string selectedCity = string.Empty;
+        public string SelectedCity
+        {
+            get => selectedCity;
+            set
+            {
+                if(selectedCity != value)
+                {
+                    selectedCity = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private int numberOfGuests;
+        public int NumberOfGuests
+        {
+            get => numberOfGuests;
+            set
+            {
+                if(numberOfGuests != value)
+                {
+                    numberOfGuests = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string language = string.Empty;
+        public string Language
+        {
+            get => language;
+            set
+            {
+                if(language != value)
+                {
+                    language = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private int duration;
+        public int Duration
+        {
+            get => duration;
+            set
+            {
+                if(duration != value)
+                {
+                    duration = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
 
         public RelayCommand SearchCommand { get; }
         public RelayCommand ResetCommand { get; }
 
 
-        private LocationController locationController;
+        private LocationService locationService;
+        private TourService tourService;
         public TourSearchPageViewModel()
         {
-            locationController = new LocationController();
+            locationService = new LocationService();
+            tourService = new TourService();
 
-            Tours = new ObservableCollection<Tour>((new TourService(new TourCSVRepository(new KeyPointCSVRepository(), new LocationCSVRepository()))).GetAll());
+            Tours = new ObservableCollection<Tour>(new TourService().GetAll());
 
-            States = locationController.GetStates();
+            States = new ObservableCollection<string>(locationService.GetStates());
             Cities = new ObservableCollection<string>();
 
             SearchCommand = new RelayCommand(Search_Execute, CanSearch);
@@ -63,24 +118,35 @@ namespace SIMS_HCI_Project_Group_5_Team_B.WPF.ViewModel.GuideGuest
 
 
 
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
 
+
+        //Commands
         public bool CanSearch()
         {
-            return NumberOfGuests > 0 && Duration > 0;
+            return NumberOfGuests > 0 && Duration >= 0;
         }
         public void Search_Execute()
         {
-            //Search Tours
+            Tours.Clear();
+            foreach(var tour in tourService.Search(new Location(SelectedState, SelectedCity), Language, Duration, NumberOfGuests)){
+                Tours.Add(tour);
+            }
         }
 
         public void Reset_Execute()
         {
             NumberOfGuests = 0;
             Duration = 0;
-            Language = String.Empty;
-            SelectedCity = String.Empty;
-            SelectedState = String.Empty;
+            Language = string.Empty;
+            SelectedCity = string.Empty;
+            SelectedState = null;//Clears the combobox
+            SelectedState = string.Empty;//Enables to search without setting
         }
     }
 }
