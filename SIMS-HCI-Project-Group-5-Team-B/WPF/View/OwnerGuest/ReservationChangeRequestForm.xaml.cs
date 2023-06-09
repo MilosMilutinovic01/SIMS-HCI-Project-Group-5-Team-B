20b326682eebd60ba9af1b575e0709a853514540
Jelena Kovač <jelena.kovac765@gmail.com>
2023-06-09 21:16:15 +0200

    ReservationChangeRequest DEMO

--- SIMS-HCI-Project-Group-5-Team-B/WPF/View/OwnerGuest/ReservationChangeRequestForm.xaml.cs
+++ SIMS-HCI-Project-Group-5-Team-B/WPF/View/OwnerGuest/ReservationChangeRequestForm.xaml.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,12 +25,34 @@ namespace SIMS_HCI_Project_Group_5_Team_B.WPF.View
     /// </summary>
     public partial class ReservationChangeRequestForm : Window
     {
-        
+        SingleReservationViewModel selectedReservationView;
         public ReservationChangeRequestForm(ObservableCollection<ReservationChangeRequest> ReservaitionChangeRequests,SingleReservationViewModel selectedReservationView, ReservationChangeRequestService reservationChangeRequestService, ReservationService reservationService)
         {
             InitializeComponent();
             this.DataContext = new ReservationChangeRequestViewModel(ReservaitionChangeRequests, selectedReservationView, reservationChangeRequestService, reservationService);
+            this.selectedReservationView = selectedReservationView;
         }
 
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(1000);
+            StartDP.Text = "12/12/2023";
+            await Task.Delay(2000);
+            EndDP.Text = "12/29/2023";
+            await Task.Delay(2000);
+            //simulacija pritiska dugmeta
+            SaveBtn.Background = Brushes.LightBlue;
+            await Task.Delay(500);
+            SaveBtn.Background = new SolidColorBrush(Color.FromRgb(162,162,200));
+            await Task.Delay(500);
+           MessageBoxResult result = MessageBox.Show("Request sent!","Reservation Change Request", MessageBoxButton.OK, MessageBoxImage.Information);
+            if(result == MessageBoxResult.OK)
+            {
+                StartDP.SelectedDate = selectedReservationView.Reservation.StartDate;
+                EndDP.SelectedDate= selectedReservationView.Reservation.EndDate;
+                 MessageBox.Show("Demo Finished", "Reservation Change Request", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+        }
     }
 }
